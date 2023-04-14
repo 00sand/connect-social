4d801dfba9bc4e94ac1b17893bee529fda4cc101
00sand <111708646+00sand@users.noreply.github.com>
2023-04-13 19:59:22 -0700

    bulma

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import 'bulma/css/bulma.css'
+
 import { AuthContext } from "../helper/AuthContext";
 import { RxCrossCircled } from 'react-icons/rx';
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { AuthContext } from "../helper/AuthContext";
 import { FiThumbsUp } from 'react-icons/fi';
 import axios from "axios";
-import 'bulma/css/bulma.css'
+
 import '../App.css';
 
 
